a22d268e5bd5fa1482cd170a5fbbaf1c0d032301
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2022-11-07 16:25:23 -0600

    idk why the last one didn't work but ok

--- ci.py
+++ ci.py
@@ -51,10 +51,6 @@ with ZipFile(ZIP, "w", ZIP_LZMA) as w:
         p = Path(file)
         w.write(p.absolute(), p.name)
     os.chdir(HOME_DIR)
-    files = glob("*.bin")
-    for file in files:
-        p = Path(file)
-        w.write(p.absolute(), f"CustomCode\\{p.name}")
     for entry,_,_ in os.walk("SMG2PTD"):
         if entry != "SMG2PTD":
             entry = Path(entry)
@@ -62,3 +58,7 @@ with ZipFile(ZIP, "w", ZIP_LZMA) as w:
                 idx = str(file).index("\\")
                 name = str(file)[idx+1:]
                 w.write(file, name)
+    files = glob("*.bin")
+    for file in files:
+        p = Path(file)
+        w.write(p.absolute(), f"CustomCode\\{p.name}")
