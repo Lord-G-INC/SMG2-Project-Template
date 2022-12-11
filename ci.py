32aa0b7ee03fbe00226539a2f7c447560a34ba1c
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2022-12-10 22:47:50 -0600

    CI rework (zips now get published + gle builds)

--- ci.py
+++ ci.py
@@ -21,17 +21,6 @@ SYATI_SETUP = dep("deps\\syatisetup.exe", "SyatiSetup").absolute()
 HOME_DIR = dep(__file__, "ci.py").parent.absolute()
 ZIP = Path("PT Debug.zip").absolute()
 
-os.chdir("deps")
-
-if os.path.exists("CodeWarrior") != True and os.path.exists("Kamek") != True:
-    subprocess.run(SYATI_SETUP)
-
-os.chdir(HOME_DIR)
-
-subprocess.run(f"python {BUILD}", shell=True)
-
-os.chdir(SYATI)
-
 if os.path.exists("deps") != True:
     os.mkdir("deps")
 
@@ -40,25 +29,89 @@ os.chdir("deps")
 if os.path.exists("CodeWarrior") != True and os.path.exists("Kamek") != True:
     subprocess.run(SYATI_SETUP)
 
-os.chdir(SYATI)
+os.chdir(HOME_DIR)
 
-subprocess.run("python buildloader.py")
+def no_gle():
+    subprocess.run(f"python {BUILD} --nogle", shell=True)
 
-with ZipFile(ZIP, "w", ZIP_LZMA) as w:
-    os.chdir("loader")
-    files = glob("*.xml")
-    for file in files:
-        p = Path(file)
-        w.write(p.absolute(), p.name)
-    os.chdir(HOME_DIR)
-    for entry,_,_ in os.walk("SMG2PTD"):
-        if entry != "SMG2PTD":
-            entry = Path(entry)
-            for file in entry.glob("*.*"):
-                idx = str(file).index("\\")
-                name = str(file)[idx+1:]
-                w.write(file, name)
-    files = glob("*.bin")
-    for file in files:
-        p = Path(file)
-        w.write(p.absolute(), f"CustomCode\\{p.name}")
+    bins = glob("*.bin")
+
+    # Safety check
+
+    if len(bins) == 0:
+        print("Some CodeWarrior or Kamek error occured. Check above for it!")
+        exit(1)
+
+    os.chdir(SYATI)
+
+    subprocess.run("python buildloader.py")
+
+    with ZipFile(ZIP, "w", ZIP_LZMA) as w:
+        os.chdir("loader")
+        files = glob("*.xml")
+        for file in files:
+            p = Path(file)
+            w.write(p.absolute(), p.name)
+        os.chdir(HOME_DIR)
+        for entry,_,_ in os.walk("SMG2PTD"):
+            if entry != "SMG2PTD":
+                entry = Path(entry)
+                for file in entry.glob("*.*"):
+                    idx = str(file).index("\\")
+                    name = str(file)[idx+1:]
+                    w.write(file, name)
+        files = glob("*.bin")
+        for file in files:
+            p = Path(file)
+            w.write(p.absolute(), f"CustomCode\\{p.name}")
+
+def with_gle():
+    subprocess.run(f"python {BUILD} --gle", shell=True)
+
+    bins = glob("*.bin")
+
+    # Safety check
+
+    if len(bins) == 0:
+        print("Some CodeWarrior or Kamek error occured. Check above for it!")
+        exit(1)
+
+    os.chdir(SYATI)
+
+    subprocess.run("python buildloader.py")
+
+    with ZipFile(ZIP.with_name("PT Debug with GLE.zip"), "w", ZIP_LZMA) as w:
+        os.chdir("loader")
+        files = glob("*.xml")
+        for file in files:
+            # GLE does not currently support Korean or Tiawan regions.
+            if "KOR" in file or "TWN" in file:
+                continue
+            p = Path(file)
+            w.write(p.absolute(), p.name)
+        os.chdir(HOME_DIR)
+        for entry,_,_ in os.walk("SMG2PTD"):
+            if entry != "SMG2PTD":
+                entry = Path(entry)
+                for file in entry.glob("*.*"):
+                    # GLE does not currently support Korean or Tiawan regions.
+                    if "K" in file.name or "W" in file.name:
+                        continue
+                    idx = str(file).index("\\")
+                    name = str(file)[idx+1:]
+                    w.write(file, name)
+        files = glob("*.bin")
+        for file in files:
+            # GLE does not currently support Korean or Tiawan regions.
+            if "KOR" in file or "TWN" in file:
+                continue
+            p = Path(file)
+            w.write(p.absolute(), f"CustomCode\\{p.name}")
+
+no_gle()
+
+files = glob("*.bin")
+for file in files:
+    os.remove(file)
+
+with_gle()
