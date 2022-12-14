4a7dbad67b0030f3d57f113040e99b6f500064d9
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2022-12-14 17:29:01 -0600

    Optimize JUTHolder

--- include/pt/Extras/MiscExtensions.h
+++ include/pt/Extras/MiscExtensions.h
@@ -10,11 +10,29 @@ class JUTHolder {
         u64 ladr = (u64)Textures[pos];
         u64 radr = (u64)texture;
         if (ladr != radr) {
-            delete Textures[pos];
+            if (ladr != NULL) {
+                OSReport("Texture %d has been deleted.\n", pos);
+                delete Textures[pos];
+            } else {
+                OSReport("Texture %d now has a value.\n", pos);
+            }
             Textures[pos] = texture;
+        } else {
+            OSReport("Address did not change, not changing Texture %d\n", pos);
+            delete texture;
         }
     }
     JUTTexture* operator[](u8 pos) {
         return Textures[pos];
+    }
+    JUTHolder() {
+        for (int i = 0; i < 4; i++) {
+            Textures[i] = 0;
+        }
+    }
+    ~JUTHolder() {
+        for (int i = 0; i < 4; i++) {
+            delete Textures[i];
+        }
     }
 };
