744f99cc1ff09f38e04c1863773b65dcf732defe
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2023-01-11 19:48:52 -0600

    fix cw issues

--- include/pt/Extras/JUTHolder.h
+++ include/pt/Extras/JUTHolder.h
@@ -7,11 +7,38 @@ template <s32 SIZE>
 class JUTHolder {
     public:
     JUTTexture* Textures[SIZE];
-    void SetTexture(u8 pos, JUTTexture* texture);
-    JUTTexture* operator[](u8 pos);
-    JUTTexture* getTexture(u8 pos);
-    JUTHolder();
-    ~JUTHolder();
-
-    s32 mSize;
+    void SetTexture(u8 pos, JUTTexture* texture) {
+        // Get Address of pointers, delete old one if adresses dont match, otherwise do nothing.
+        u64 ladr = (u64)Textures[pos];
+        u64 radr = (u64)texture;
+        if (ladr != radr) {
+            if (ladr != NULL) {
+                OSReport("Texture %d has been deleted.\n", pos);
+                delete Textures[pos];
+            } else {
+                OSReport("Texture %d now has a value.\n", pos);
+            }
+            Textures[pos] = texture;
+        } else {
+            OSReport("Address did not change, not changing Texture %d\n", pos);
+            delete texture;
+        }
+    }
+    JUTTexture* operator[](u8 pos) {
+        return Textures[pos];
+    }
+    JUTTexture* getTexture(u8 pos) {
+        return operator[](pos);
+    }
+    JUTHolder() {
+        OSReport("Size: %d\n", SIZE);
+        for (int i = 0; i < SIZE; i++) {
+            Textures[i] = 0;
+        }
+    }
+    ~JUTHolder() {
+        for (int i = 0; i < SIZE; i++) {
+            delete Textures[i];
+        }
+    }
 };
