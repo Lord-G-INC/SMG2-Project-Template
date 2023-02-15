5618508ebb90ca4d1744578b70d3d9623b79d1d5
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2023-02-14 20:40:41 -0600

    somewhat overload reciveMessage

--- source/pt/MapObj/BlueCoinSystem/BlueCoin.cpp
+++ source/pt/MapObj/BlueCoinSystem/BlueCoin.cpp
@@ -4,3 +4,11 @@ void BlueCoin::init(const JMapInfoIter& rIter) {
     MR::getJMapInfoArg0NoInit(rIter, (s32*)&mID);
     mInstance = gBlueCoinMainager[mID];
 }
+
+bool BlueCoin::receiveMessage(u32 msg, HitSensor* pSender, HitSensor* pReciver) {
+    if (MR::isMsgItemGet(msg)) {
+        mInstance->mIsCollected = true;
+        // TODO: Make the coin clear instead, like a star.
+        MR::hideModel(this);
+    }
+}
