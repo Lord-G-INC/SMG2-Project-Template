c406ac6b82fa0f03c33a480d8747c0db3fa6f8f6
Evanbowl <evansbowl@gmail.com>
2023-06-07 20:48:29 -0400

    update restart button

--- source/pt/Extras/PauseMenuNewButton.cpp
+++ source/pt/Extras/PauseMenuNewButton.cpp
@@ -37,7 +37,7 @@ void ForceToWaitNewButton(PauseMenu* pPauseMenu) {
     pPauseMenu->mButtonNew->forceToWait();
 }
 
-kmWrite32(0x80487504, 0x60000000); // nop
+kmWrite32(0x80487504, 0x60000000); // nop (Skip overwriting r3)
 kmCall(0x80487508, ForceToWaitNewButton); // Call
 
 void AppearNewButton(PauseMenu* pPauseMenu) {
@@ -45,14 +45,14 @@ void AppearNewButton(PauseMenu* pPauseMenu) {
     pPauseMenu->mButtonNew->appear();
 }
 
-kmWrite32(0x80487560, 0x7FE3FB78); // mr r3, r31
+kmWrite32(0x80487560, 0x7FE3FB78); // mr r3, r31 (PauseMenu* into r3)
 kmCall(0x80487564, AppearNewButton); // Call
 
 bool IsNewButtonPointingTrigger(PauseMenu* pPauseMenu) {
     return pPauseMenu->mButtonTop->isPointingTrigger() || pPauseMenu->mButtonNew->isPointingTrigger();
 }
 
-kmWrite32(0x80487714, 0x7F63DB78); // mr r3, r27
+kmWrite32(0x80487714, 0x7F63DB78); // mr r3, r27 (PauseMenu* into r3)
 kmCall(0x80487720, IsNewButtonPointingTrigger);
 
 bool IsNewButtonPressed(PauseMenu* pPauseMenu) {
@@ -71,24 +71,24 @@ bool IsNewButtonPressed(PauseMenu* pPauseMenu) {
     return isPressed;
 }
 
-kmWrite32(0x804877B4, 0x7F63DB78); // mr r3, r27
+kmWrite32(0x804877B4, 0x7F63DB78); // mr r3, r27 (PauseMenu* into r3)
 kmCall(0x804877C0, IsNewButtonPressed); // Call
 
 const char* SysInfoWindowStr(bool isValid, PauseMenu* pPauseMenu) {
     return pPauseMenu->mIsUsedNewButton ? "PauseMenu_ConfirmRestartStage" : isValid ? "PauseMenu_ConfirmEndGame" : "PauseMenu_ConfirmBackWorldMap";
 }
 
-kmWrite32(0x80487C34, 0x7FE4FB78); // mr r4, r31
+kmWrite32(0x80487C34, 0x7FE4FB78); // mr r4, r31 (PauseMenu* into r4)
 kmCall(0x80487C38, SysInfoWindowStr); // Call
-kmWrite32(0x80487C3C, 0x7C641B78); // mr r4, r3
-kmWrite32(0x80487C40, 0x4800000C); // b 0xC
+kmWrite32(0x80487C3C, 0x7C641B78); // mr r4, r3 (Store returned const char* into r4)
+kmWrite32(0x80487C40, 0x4800000C); // b 0xC (Skip useless instructions)
 
 void DisappearNewButton(PauseMenu* pPauseMenu) {
     pPauseMenu->mButtonTop->disappear();
     pPauseMenu->mButtonNew->disappear();
 }
 
-kmWrite32(0x80487B10, 0x7FC3F378); // mr r3, r30
+kmWrite32(0x80487B10, 0x7FC3F378); // mr r3, r30 (PauseMenu* into r3)
 kmCall(0x80487B14, DisappearNewButton); // Call
 
 bool IsNewButtonTimingForSelectedSE(PauseMenu* pPauseMenu) {
@@ -99,9 +99,9 @@ bool IsNewButtonTimingForSelectedSE(PauseMenu* pPauseMenu) {
 }
 
 kmWrite32(0x804879B0, 0x809E0034); // lwz r4, 0x34(r30)
-kmWrite32(0x804879B4, 0x2C040001); // cmpwi r4, 0x1
+kmWrite32(0x804879B4, 0x2C040001); // cmpwi r4, 1
 
-kmWrite32(0x804879C0, 0x2C000001); // cmpwi r0, 0x1
+kmWrite32(0x804879C0, 0x2C000001); // cmpwi r0, 1
 
 kmWrite32(0x804879C8, 0x7FC3F378); // mr r3, r30
 kmCall(0x804879CC, IsNewButtonTimingForSelectedSE); // Call
@@ -111,15 +111,17 @@ bool IsNewButtonDecidedWait(PauseMenu* pPauseMenu) {
     return pPauseMenu->mButtonBottom->isDecidedWait() || pPauseMenu->mButtonNew->isDecidedWait();
 }
 
-kmWrite32(0x80487A00, 0x7FC3F378); // mr r3, r30
+kmWrite32(0x80487A00, 0x7FC3F378); // mr r3, r30 (PauseMenu* into r3)
 kmCall(0x80487A04, IsNewButtonDecidedWait); // Call
 
-void DoNewButtonAction(bool isUsedNewButton, bool isValid) {
+void DoNewButtonAction(PauseMenu* pPauseMenu, bool isValid) {
     MR::startSystemWipeCircleWithCaptureScreen(0x5A);
-
-    if (isUsedNewButton) {
-		GameSequenceFunction::requestChangeScenarioSelect(MR::getCurrentStageName());
-		GameSequenceFunction::requestChangeStage(MR::getCurrentStageName(), MR::getCurrentScenarioNo(), MR::getCurrentSelectedScenarioNo(), *(JMapIdInfo*)0);
+    
+    if (pPauseMenu->mIsUsedNewButton) {
+        const char* currStage = MR::getCurrentStageName();
+        s32 currScenario = MR::getCurrentScenarioNo();
+        GameSequenceFunction::requestChangeScenarioSelect(currStage);
+		GameSequenceFunction::requestChangeStage(currStage, currScenario, MR::getCurrentSelectedScenarioNo(), *(JMapIdInfo*)0);
     }
     else {
         if (isValid)
@@ -129,8 +131,8 @@ void DoNewButtonAction(bool isUsedNewButton, bool isValid) {
     }
 }
 
-kmWrite32(0x80487CA4, 0x7C641B78); // mr r4, r3
-kmWrite32(0x80487CA8, 0x889F005A); // lbz r4, 0x5A(r31)
+kmWrite32(0x80487CA4, 0x7C641B78); // mr r4, r3 (isValid into r4)
+kmWrite32(0x80487CA8, 0x7FE3FB78); // mr r3, r31 (PauseMenu* into r3)
 kmCall(0x80487CAC, DoNewButtonAction); // Call
-kmWrite32(0x80487CB0, 0x48000008); // b 0xC
+kmWrite32(0x80487CB0, 0x48000008); // b 0x8 (Skip useless instructions)
 #endif
