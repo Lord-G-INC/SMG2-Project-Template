6b75e161b8105f5fe2e0b2e7a2748a6a4ac31349
AwesomeTMC <38051573+AwesomeTMC@users.noreply.github.com>
2023-07-10 03:51:38 -0600

    obj_arg3 Star Ball
    Music to play: -1 Default Behavior, 0 Slider, 1 Don't play, 2 Normal Music

--- include/pt/Extras/PowerStarColors.h
+++ include/pt/Extras/PowerStarColors.h
@@ -6,5 +6,6 @@
 namespace pt {
 	s32 getPowerStarColor(const char *pStage, s32 scenarioId);
 	s32 getPowerStarColorCurrentStage(s32 scenarioId);
+	void TamakoroCustomPowerStarColorsParticles(LiveActor* pActor);
 };
 #endif
