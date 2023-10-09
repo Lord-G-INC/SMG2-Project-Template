c7ad773f643662401d0dd48882889eb079795422
Evanbowl <74565954+Evanbowl@users.noreply.github.com>
2023-10-08 21:49:48 -0400

    Better calculation

--- source/pt/Util/TalkUtil.cpp
+++ source/pt/Util/TalkUtil.cpp
@@ -12,21 +12,10 @@ namespace pt {
 		pTalkCtrl->setMessageArg(tagArg);
 	}
 
-	// if this were possible through MSBT, I'd do it that way
-	// this code also sucks. i need to figure out how to handle this better
 	void setTextBoxArgNumberNumberFontRecursive(LayoutActor* pLayout, const char* paneName, s32 num, s32 index) {
-		s32 charnum = log10(num)+1;
-		const char* str2;
-
-			if (charnum == 1)
-				str2 = "\u0004\u0002";
-			else if (charnum == 2)
-				str2 = "\u0006\u0004";
-			else if (charnum == 3)
-				str2 = "\u0008\u0006";
-
-		const wchar_t* str = new wchar_t[9];
-		swprintf((wchar_t*)str, 9, L"\u000E\u000A\u0001%s%d", str2, num);
+		s32 digits = log10(num)+1;
+		const wchar_t* str = new wchar_t[12];
+		swprintf((wchar_t*)str, 12, L"\u000E\u000A\u0001%lc%lc%d", 2+digits*2, digits*2, num);
         MR::setTextBoxArgStringRecursive(pLayout, paneName, str, index);
 		delete str;
 	}
