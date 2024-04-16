22803ba2fdb005404ce505ee0a05b98ae8a7064e
nico-apps <nicomarikucza@googlemail.com>
2024-04-16 09:20:14 +0200

    Apparently a hardware reset is not enough

--- source/pt/Extras/ModSpecific/SMG63/SMG63Misc.cpp
+++ source/pt/Extras/ModSpecific/SMG63/SMG63Misc.cpp
@@ -2,23 +2,25 @@
 #include "Game/Util/BgmUtil.h"
 #include "Game/Util/SoundUtil.h"
 #include "kamek/hooks.h"
-bool gIsFileSelectUsable = false;
+bool gIsFileSelectUsable;
 
 // This is experimental, so it is currently only used for SMG63.
 void playFileSelectMusic () {
-    MR::stopSystemSE("SE_SY_LV_TITLE_BGM", 0);
-    /*MR::stopSystemSE("SE_SY_LV_TITLE_BGM_RHYTHM", 0);
-    MR::stopSystemSE("BGM_TITLE", 0);
-    MR::stopSystemSE("MBGM_TITLE", 0);*/
+    MR::stopSystemSE("SE_SY_LV_TITLE_BGM", 18);
     MR::startSubBGM("STM_TITLE", false);
     gIsFileSelectUsable = true;
 }
 kmBranch(0x8024CB90, playFileSelectMusic);
 
-void controlFileSelectBGM (const char *seName, s32, s32) {
+void controlFileSelectBGM (const char *, s32, s32) {
     if (!gIsFileSelectUsable) 
-        MR::startSystemLevelSE(seName, -1, -1);
+        MR::startSystemLevelSE("SE_SY_LV_TITLE_BGM", -1, -1);
 }
-
 kmCall(0x80251400, controlFileSelectBGM);
+
+void onInitFileSelect () {
+    gIsFileSelectUsable = false;
+}
+kmBranch(0x8024F480, onInitFileSelect);
+
 #endif
